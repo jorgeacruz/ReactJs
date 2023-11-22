60170261b43496aeeb316428a34999c09166b7b6
Jorge Cruz <j.alexcruz@gmail.com>
2023-11-22 15:13:51 -0300

    Buscador de Cep

--- buscador-cep/src/App.tsx
+++ buscador-cep/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex items-center justify-center mx-auto bg-Primary">
+      <div className='w-full max-w-4xl flex flex-col items-center gap-3 '>
+        <img src='https://i.pinimg.com/originals/83/23/8c/83238c8117658fe5ee2cafb479b0eb68.png' alt="" />
+        <input type="text" className='bg-transparent border border-Secondary w-full max-w-sm p-2 rounded-lg text-center' placeholder='Pesquise seu Cep'/>
+        <button className='bg-black text-white p-2 w-full max-w-sm rounded-lg hover:bg-Secondary duration-300 hover:scale-95'>
+          Pesquisar Cep
+        </button>
+
+        <div className='w-full max-w-sm flex flex-col items-center gap-4 py-8 mt-10 bg-Secondary text-white rounded-lg'>
+          <h1 className="text-2xl font-bold">21060-130</h1>
+          <p>Rua Teixeira Franco 129</p>
+          <p>Ramos - Rio de Janeiro</p>
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ buscador-cep/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "Primary":"#FFD525",
+        "Secondary":" #01325A",
+        "Default":"#fff"
+      }
+    },
+  },
+  plugins: [],
+}
+
