0fda5ad5bde71f30eaf5b69129be1f7a265dc1a7
Jorge Cruz <j.alexcruz@gmail.com>
2023-11-23 21:26:44 -0300

    Config tailwind and delete some files

--- questions/tailwind.config.js
+++ questions/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "Primary":"#fff",
+        "Secondary":"#000"
+      }
+    },
   },
   plugins: [],
 }
