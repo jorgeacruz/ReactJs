4294a663b9a051086963601a3fd24a137a087a10
Jorge Cruz <j.alexcruz@gmail.com>
2023-11-27 20:13:19 -0300

    Weather App - First Commit

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 
 export default function App() {
+
+  const [city, setCity] = useState();
+  
   return (
     <div className="w-full h-screen bg-black flex items-center justify-center">
       <h1 className='text-white'>Weather App - api</h1>
