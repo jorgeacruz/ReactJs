2f2acf99316be3d571aa8b7a46d167bb7f09be9d
Jorge Cruz <j.alexcruz@gmail.com>
2023-11-27 20:10:19 -0300

    Weather App - First Commit

---
+++ weather-app/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="w-full h-screen bg-black flex items-center justify-center">
+      <h1 className='text-white'>Weather App - api</h1>
+    </div>
+  );
+}
+
+
