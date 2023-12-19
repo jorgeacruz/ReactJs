4b0af6c56dd83bd2e12cdff9734583f897f73ea5
Jorge Cruz <j.alexcruz@gmail.com>
2023-12-19 19:38:31 -0300

    some changes

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+
+import clouds from './assets/video/clouds.mp4'
 
 export default function App() {
 
-  const [city, setCity] = useState();
+  
   
   return (
-    <div className="w-full h-screen bg-black flex items-center justify-center">
-      <h1 className='text-white'>Weather App - api</h1>
+    <div className="w-full h-screen bg-yellow-900 flex flex-col items-center justify-center gap-5">
+     <div className='w-full max-w-7xl overlay border border-white rounded-xl '>
+        <video src={clouds} autoPlay loop muted />
+     </div>
     </div>
   );
 }
